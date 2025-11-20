3bca33a57dfdf038ed0f0f6c23fd2ece6bb38849
Decen Ng <decen.ng@thedoodlepeople.com>
2025-11-20 14:00:03 +0800

    Add interactable tablet and clean up scripts

---
+++ Assets/_Scripts/Assembly/InteractCanvas.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractCanvas : MonoBehaviour
+{
+    public GameObject popUp;
+
+    public void Interact()
+    {
+        popUp.SetActive(!popUp.activeSelf);
+    }
+}
---
+++ Assets/_Scripts/Assembly/ScrollScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScreen : MonoBehaviour
+{
+    public GameObject parentObject;
+
+    public void NextPage()
+    {
+
+    }
+
+    public void PreviousPage()
+    {
+
+    } 
+}
