c20c2c6da8f49eeb4fdc4ecbf3916eb6382033cc
Decen Ng <decen.ng@thedoodlepeople.com>
2025-11-20 19:14:19 +0800

    tablet canvas

--- Assets/_Scripts/Assembly/ScrollScreen.cs
+++ Assets/_Scripts/Assembly/ScrollScreen.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class ScrollScreen : MonoBehaviour
 {
+    public GameObject backButton;
+    public GameObject nextButton;
     public GameObject parentObject;
+    GameObject[] images;
+    int currentPage;
 
-    public void NextPage()
+	private void Start()
+	{
+        int size = parentObject.transform.childCount;
+        images = new GameObject[size];
+
+		for (int i = 0; i < size; i++)
+        {
+            images[i] = parentObject.transform.GetChild(i).gameObject;
+        }
+
+        currentPage = 0;
+	}
+
+	public void NextPage()
     {
+        if (currentPage + 1 < images.Length)
+        {
+            if (!backButton.activeSelf)
+            {
+                backButton.SetActive(true);
+            }
 
+			parentObject.transform.GetChild(currentPage).gameObject.SetActive(false);
+			currentPage++;
+			parentObject.transform.GetChild(currentPage).gameObject.SetActive(true);
+
+            if(currentPage == images.Length - 1)
+            {
+                nextButton.SetActive(false);
+            }
+		}
+        
     }
 
     public void PreviousPage()
     {
+        if(currentPage != 0)
+        {
+            if (!nextButton.activeSelf)
+            {
+                nextButton.SetActive(true);
+            }
 
-    } 
+			parentObject.transform.GetChild(currentPage).gameObject.SetActive(false);
+			currentPage--;
+			parentObject.transform.GetChild(currentPage).gameObject.SetActive(true);
+			
+            if(currentPage == 0)
+            {
+                backButton.SetActive(false);
+            }
+		}
+
+
+	} 
 }
